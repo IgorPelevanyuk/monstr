301acd338d16e8fa8d2a1ef78f2630821dc726df
Igor Pelevanyuk <gavelock@gmail.com>
2016-12-19 13:16:01 +0300

    UPDATE: datetime_to_epoch added, build_url added

--- Monstr/Core/Utils.py
+++ Monstr/Core/Utils.py
@@ -20,5 +20,20 @@ def epoch_to_datetime(seconds):
     new_time = datetime.datetime.utcfromtimestamp(float(seconds)).replace(tzinfo=pytz.utc)
     return new_time
 
+def datetime_to_epoch(timestamp):
+    """Convert datetime timestamp to epoch seconds(double). Miliseconds comes as decimal.
+    Return example:
+    >>> datetime_to_epoch(x)
+    1479730200.0"""
+    epoch = datetime.datetime(1970, 1, 1, tzinfo=pytz.utc)
+    result = (timestamp - epoch).total_seconds()
+    return result
+
 def get_UTC_now():
     return datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+
+def build_URL(baseURL, params):
+    URL = baseURL
+    for param in params:
+        URL = URL.replace('<' + param + '>', str(params[param]))
+    return URL
