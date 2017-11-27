e688a7c23abd9db668300773d78c1ba1b5b9669c
Igor Pelevanyuk <gavelock@gmail.com>
2017-11-27 12:23:18 +0300

    FIX: Tests fix according to current REST

--- Monstr/Modules/SSB/test_SSB.py
+++ Monstr/Modules/SSB/test_SSB.py
@@ -1,19 +1,22 @@
 from Monstr.Core import Runner
 
+
 def test_SSB_initial():
     modules = Runner.get_modules()
     assert 'SSB' in modules
     if 'SSB' in modules:
-         modules['SSB'].main()
+        modules['SSB'].main()
+
 
 def test_SSB_additional():
     modules = Runner.get_modules()
     assert 'SSB' in modules
     if 'SSB' in modules:
-         modules['SSB'].main()
+        modules['SSB'].main()
+
 
 def test_RESTs():
     from Monstr.Modules.SSB.SSB import SSB
     obj = SSB()
     for rest_name in obj.rest_links:
-        obj.rest_links[rest_name](obj, {})
+        obj.rest_links[rest_name]({})
