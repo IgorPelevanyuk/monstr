78f9a44375e99f722faddb009c6e302101e7f127
Igor Pelevanyuk <gavelock@gmail.com>
2017-12-20 11:18:30 +0300

    TEST: SSB tests improved

--- Monstr/Modules/SSB/test_SSB.py
+++ Monstr/Modules/SSB/test_SSB.py
@@ -3,16 +3,12 @@ from Monstr.Core import Runner
 
 def test_SSB_initial():
     modules = Runner.get_modules()
-    assert 'SSB' in modules
-    if 'SSB' in modules:
-        modules['SSB'].main()
+    modules['SSB'].main()
 
 
 def test_SSB_additional():
     modules = Runner.get_modules()
-    assert 'SSB' in modules
-    if 'SSB' in modules:
-        modules['SSB'].main()
+    modules['SSB'].main()
 
 
 def test_RESTs():
