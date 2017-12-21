dddcc019354a5a102af42bb78f5d94e4040a52ff
Igor Pelevanyuk <gavelock@gmail.com>
2017-12-21 10:16:45 +0300

    TEST: coveralls increase SSB coverage

--- Monstr/Modules/SSB/test_SSB.py
+++ Monstr/Modules/SSB/test_SSB.py
@@ -36,5 +36,9 @@ def test_SSB_manual_update():
 def test_RESTs():
     from Monstr.Modules.SSB.SSB import SSB
     obj = SSB()
+    # last_row = self.db_handler.get_session().query(DB.func.max(self.tables['main'].c.time).label("max_time")).one()
+    print obj.db_handler
+    print obj.tables['main']
+    print obj.tables['main'].c.time
     for rest_name in obj.rest_links:
         obj.rest_links[rest_name]({})
