daa4094cc23556f0ebaeb5b4455fbc9643132e31
Igor Pelevanyuk <gavelock@gmail.com>
2018-11-27 12:42:39 +0300

    TEST: mock email_send

--- Monstr/Modules/CMSJobStatus/test_CMSJobStatus.py
+++ Monstr/Modules/CMSJobStatus/test_CMSJobStatus.py
@@ -1,17 +1,21 @@
 import mock
 
+def pass_method():
+    pass
 
+mock.patch('Monstr.Core.Utils.send_email')
 def test_CMSJobStatus_beforeLastHour():
     import datetime
     import pytz
     import Monstr.Modules.CMSJobStatus.CMSJobStatus as testedModule
 
-    with mock.patch("Monstr.Modules.CMSJobStatus.CMSJobStatus.Utils.get_UTC_now") as MockClass:
-        MockClass.return_value = datetime.datetime.utcnow().replace(tzinfo=pytz.utc) - datetime.timedelta(hours=1)
-        testedModule.main()
+    with mock.patch('Monstr.Modules.CMSJobStatus.CMSJobStatus.Utils.send_email') as MockEmail:
+        MockEmail.return_value = None
+        with mock.patch("Monstr.Modules.CMSJobStatus.CMSJobStatus.Utils.get_UTC_now") as MockClass:
+            MockClass.return_value = datetime.datetime.utcnow().replace(tzinfo=pytz.utc) - datetime.timedelta(hours=1)
+            testedModule.main()
 
-
-def test_CMSJobStatus_addLastHour():
+def test_CMSJobStatus_addLastHour():    
     import Monstr.Modules.CMSJobStatus.CMSJobStatus as testedModule
     testedModule.main()
 
